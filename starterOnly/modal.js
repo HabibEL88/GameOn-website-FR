84a7f0024aed9eb9875b599be5fb9d13c9082425
HABIB <elmouini.habib@gmail.com>
2022-07-24 21:38:16 +0200

    close moadale ok

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -21,3 +21,36 @@ function launchModal() {
 }
 
 
+// COMMENTAIRE TYPE
+// Je veux ajouter une fonctionnalité
+// 1/ Je récupére l'élément => querySelector, querySelectorAll
+// 2/ Je crée une fonction JS qui fait ce que je veux
+// 3/ J'appelle cette fonction de la bonne manière
+// 4/ Je vérifie les cas limites => j'ai pas l'élément, c'est null, undefined, etc.
+
+
+// Close the Modale on click on X button
+// 1/ Retrieve the close button from the DOM
+const closebutton = document.querySelector(".close");
+// 2/ Create a "Close function"
+// ajouter un événement de clique sur le bouton, qui ferme la modale
+closebutton.addEventListener("click", closeModale);
+// 3/ J'écris la fonction de fermeture de la modale
+function closeModale() {
+  // Je récupère la modal 
+  modalbg.style.display = "none";
+}
+
+const formulaire = document.forms["reserve"];
+const firstName = formulaire.querySelector("#first");
+console.debug(firstName);
+
+// Afficher des erreurs
+// A/ Ajoute dans ton fichier .html un message d'erreur, que tu hide par défaut, affiche si nécessaire
+// B/ Insérer dans le DOM l'erreur quand elle est là
+
+
+function validateForm() {
+  // Exemple
+  formulaire.querySelector(".error").style.display = "block"; 
+}
