ba6724f401c22113065266541a3d96476dfa4430
HABIB <elmouini.habib@gmail.com>
2022-07-28 12:48:45 +0200

    close modal +erreur first/last/email

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,8 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -41,9 +43,7 @@ function closeModale() {
   modalbg.style.display = "none";
 }
 
-const formulaire = document.forms["reserve"];
-const firstName = formulaire.querySelector("#first");
-console.debug(firstName);
+const formulaire = document.getElementById("reserve");
 
 // Afficher des erreurs
 // A/ Ajoute dans ton fichier .html un message d'erreur, que tu hide par défaut, affiche si nécessaire
@@ -51,6 +51,61 @@ console.debug(firstName);
 
 
 function validateForm() {
-  // Exemple
-  formulaire.querySelector(".error").style.display = "block"; 
+  const first = document.getElementById("first");
+  const errorFirst = document.getElementById("errorFirstName");
+  if (first.value.length < 2) {
+    errorFirst.style.display = "block";
+    errorFirst.textContent = "Veuillez remplir le firstName";
+  } 
+  else {    
+    errorFirst.style.display = "none";
+    console.log("je suis là")
+  }
+//(1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
+
+
+ const last = document.getElementById("last"); /*on crée une variable qui frecupere l'element HTML "last" */
+  const errorLastName = document.getElementById("errorLastName"); 
+  if (last.value.length < 2) {  // si seulement last.value = true si quelque chose dedans !=contraire de / || = ou / longueur 
+    errorLastName.style.display = "block";
+    errorLastName.textContent = "Veuillez remplir le lastname";
+  }
+  else {    
+    errorLastName.style.display = "none";
+  }
+
+//(2) Le champ du nom de famille a un minimum de 2 caractères / n'est pas vide.
+  const email = document.getElementById("email");
+  const errorEmail = document.getElementById("errorMail");
+  if ( !email.value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g) ) { //regex pour verification format email
+    errorEmail.style.display = "block";
+    errorEmail.textContent = "Veuillez remplir l'email";
+  }
+  else {    
+    errorEmail.style.display = "none";
+  }
+
+//(3) L'adresse électronique est valide.
+  /*const birthday = document.getElementById ("birthday");
+  if (!first.value || first.value.length >= 2) {
+    const errorFirst = document.getElementById ("errorFirstName");
+    errorFirst.style.display = "block";
+    errorFirst.textContent = "Veuillez remplir le firstName";
+  }
+  */
+ const quantity = document.getElementById("quantity");
+ const errorQuantity = document.getElementById("errorQuantity");
+ 
+ let quantityInt = parseInt(quantity.value, 10) //parseInt change string en int (mettre ",10" pour convertir en base 10)
+ console.log((typeof)quantityInt)
+if(quantityInt == ""){
+
+
 }
+
+}
+
+const validatebutton = document.querySelector(".btn-submit");
+validatebutton.addEventListener("click", validateForm);
+
+() 
